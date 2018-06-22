d9aa66ed771ea7d5032ee85e489c1398184cdf62
Casey <caclowers@gmail.com>
2018-06-22 16:24:12 -0500

    added content to DOM and added functionality to the inputs and button on the .js file

--- script.js
+++ script.js
@@ -4,5 +4,47 @@ $(document).ready(readyNow);
 
 function readyNow(){
     console.log('huhu');
-    
+    addEventHandlers();
+}
+
+//Class Employee
+class Employee{
+    constructor(firstNameVal, lastNameVal, employeeIDVal, jobTitleVal, annualSalaryVal){
+        this.firstNameVal = firstNameVal;
+        this.lastNameVal = lastNameVal;
+        this.employeeIDVal = employeeIDVal;
+        this.jobTitleVal = jobTitleVal;
+        this.annualSalaryVal = annualSalaryVal;
+    }
+}
+function addEventHandlers(){
+
+    $('#submitButton').on('click', handleSubmit);
+    //append row to DOM
+}
+
+function handleSubmit(){
+    //get input values
+    let firstNameVal = $('#firstName').val();
+    let lastNameVal = $('#lastName').val();
+    let employeeIDVal = $('#employeeID').val();
+    let jobTitleVal = $('#jobTitle').val();
+    let annualSalaryVal = $('#annualSalary').val();   
+    addRow(firstNameVal, lastNameVal, employeeIDVal, jobTitleVal, annualSalaryVal)
+}
+
+function addRow(firstNameVal, lastNameVal, employeeIDVal, jobTitleVal, annualSalaryVal){
+    //create row in memory
+    let $row = $('<tr></tr>');
+    $row.append('<td>' + firstNameVal + '</td>');
+    $row.append('<td>' + lastNameVal + '</td>');
+    $row.append('<td>' + employeeIDVal + '</td>');
+    $row.append('<td>' + jobTitleVal + '</td>');
+    $row.append('<td>' + annualSalaryVal + '</td>');
+    $('#employeeTable').append($row)
+    $('#firstName').val('');
+    $('#lastName').val('');
+    $('#employeeID').val('');
+    $('#jobTitle').val('');
+    $('#annualSalary').val('');
 }
